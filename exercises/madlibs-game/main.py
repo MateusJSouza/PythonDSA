e829bd940d9121133e5e912aaff9862cf2c89849
MateusJSouza <mateusjdev@gmail.com>
2024-08-20 15:49:30 -0300

    feat: add madlibs game exercise

---
+++ exercises/madlibs-game/main.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# jogo de palavras onde você cria uma história
+# preenchendo os espaços em branco com palavras aleatórias
+
+adjective1 = input("Digite um adjetivo (descritivo): ")
+noun1 = input("Digite um substantivo (pessoa, lugar ou coisa): ")
+adjective2 = input("Digite um adjetivo (descritivo): ")
+verb1 = input("Digite um verbo terminado com gerúndio (ando, endo, indo): ")
+adjective3 = input("Digite um adjetivo (descritivo): ")
+
+print(f"Hoje fui a um {adjective1} zoológico.")
+print(f"Em uma exposição, eu vi um {noun1}")
+print(f"{noun1} estava {adjective2} e {verb1}")
+print(f"E estava {adjective3}!")
