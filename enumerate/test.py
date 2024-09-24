ac17aba6517fce01038325b92608923a11ff0db4
MateusJSouza <mateusjdev@gmail.com>
2024-09-24 11:31:02 -0300

    feat: enumerate function

--- enumerate/test.py
+++ enumerate/test.py
@@ -2,11 +2,33 @@
 
 # A função enumerate retorna um objeto enumerado, que contém pares de índice e valor
 
-lista = ["abacate", "bola", "cachorro"]
+# Criando uma lista
+seq = ['a', 'b', 'c']
 
-for i in lista:
-    print(i)
+enumerate(seq)
 
-for i in enumerate(lista):
-    print(i)
-    
+list(enumerate(seq))
+
+# Exemplo 1: Imprimindo valores de uma lista com a função enumerate() e seus respectivos índices
+for indice, valor in enumerate(seq):
+    print(indice, valor)
+
+# Exemplo 2: Usando enumerate com uma condição dentro do loop
+for indice, valor in enumerate(seq):
+    if indice >= 2:
+        break
+    else:
+        print(valor)
+
+# Exemplo 3: Usando enumerate com uma lista de strings
+lista = ['Marketing', 'Vendas', 'TI', 'Comercial']
+for i, item in enumerate(lista):
+    print(i, item)
+
+# Exemplo 4: Usando enumerate com uma string
+for i, item in enumerate('Data Science Academy'):
+    print(i, item)
+
+# Exemplo 5: Usando enumerate com um range de números
+for i, item in enumerate(range(10)):
+    print(i, item)
